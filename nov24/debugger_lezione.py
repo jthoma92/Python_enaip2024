efb500b7d98582fddbf1be84c5728a530740f3dc
jthoma92 <jthoma92@gmail.com>
2024-11-22 19:41:22 +0100

    Add files via upload

---
+++ nov24/debugger_lezione.py
@@ -0,0 +1,32 @@
+#Ciclo 1:
+i = 0
+while i < 10:
+    print(i)
+    i += 1
+    i = 0
+"""
+#Ciclo 2
+i = 11
+while i < 10:
+    print(i)
+    i += 1
+
+#Ciclo 3
+x = 5
+y = 3
+
+#Ciclo 4
+i = 0
+while i < 10:
+    if i == 5:
+        break
+    print(i)
+    i += 1
+    break
+
+#Ciclo 5
+while x > y and x < 10:
+    print(x)
+    x -= 1
+    y += 1
+    """
