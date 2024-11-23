78a4f74829768e69074f4e6668cb4506731aa5e5
jthoma92@gmail.com <jthoma92@gmail.com>
2024-11-23 23:49:39 +0100

    Aggiornato i cicli per uso con debugger

--- nov24/debugger_lezione.py
+++ nov24/debugger_lezione.py
@@ -6,46 +6,51 @@ def divisione_con_problemi(numero1, numero2):
     return numero1
 
 #Ciclo 1:
-i = 0
-while i < 10:
-    print(i)
-    i += 1
-    i = 0
-"""
+# i = 0
+# while i < 10:
+#     print(i)
+#     i += 1
+#     i = 0
+
 #Ciclo 2
-i = 11
-while i < 10:
-    print(i)
-    i += 1
+# i = 11
+# while i < 10:
+#     print(i)
+#     i += 1
 
 #Ciclo 3
-x = 5
-y = 3
+# lista = [1, 2, 3, 4]
+# for i in range(len(lista)):
+#     if lista[i] == 3:
+#         lista.remove(3)
+#     print(lista[i])
 
 #Ciclo 4
-i = 0
-while i < 10:
-    if i == 5:
-        break
-    print(i)
-    i += 1
-    break
+# i = 0
+# while i < 10:
+#     if i == 5:
+#         break
+#     print(i)
+#     i += 1
+#     break
 
 #Ciclo 5
-while x > y and x < 10:
-    print(x)
-    x -= 1
-    y += 1
-    """
+# x = 5
+# y = 3
+# while x > y and x < 10:
+#     print(x)
+#     x -= 1
+#     y += 1
+    
 
-"""
+
 #function call debugging:
-"""
-il_mio_numero = somma_con_problemi(5, 8)
-while il_mio_numero < 10:
-    il_mio_numero =+ 1
 
-la_mia_divisione = divisione_con_problemi(10, 5)
-for numero in range(10):
-    la_mia_divisione += 1
-print(la_mia_divisione)  #dovrebbe essere 12?
+# il_mio_numero = somma_con_problemi(5, 8)
+# while il_mio_numero < 10:
+#     il_mio_numero =+ 1
+
+# la_mia_divisione = divisione_con_problemi(10, 5)
+# for numero in range(10):
+#     la_mia_divisione += 1
+# print(la_mia_divisione)  #dovrebbe essere 12
